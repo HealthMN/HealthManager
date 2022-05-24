954f530c89bb70b2c9015746b005ecce45e2ea0e
Mindohyeon <min50875@gmail.com>
2022-05-25 08:47:01 +0900

    commit

---
+++ Project.swift
@@ -0,0 +1,21 @@
+import ProjectDescription
+
+let projectName = "HealthManager"
+let organizationName = "com.tm"
+
+let project = Project(
+    name: projectName,
+    organizationName: organizationName,
+    targets: [
+        Target(
+            name: "\(projectName)",
+            platform: .iOS,
+            product: .app,
+            bundleId: "\(organizationName).\(projectName)",
+            deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
+            infoPlist: .file(path: Path("Support/Info.plist")),
+            sources: ["Source/**"],
+            resources: ["Resource/**"]
+        )
+    ]
+)
