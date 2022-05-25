72ad846a58d8c1fc593d1db58f5742f593e15e3e
Mindohyeon <min50875@gmail.com>
2022-05-25 15:08:51 +0900

    [Add] UnderLineTextField

--- Source/Scene/Login/VC/LoginVC.swift
+++ Source/Scene/Login/VC/LoginVC.swift
@@ -11,8 +11,17 @@ class LoginVC: BaseVC {
         $0.textColor = UIColor(red: 0.25, green: 0.26, blue: 0.58, alpha: 1)
     }
     
+    let emailTextLabel = UILabel().then {
+        $0.text = "Email"
+        $0.font = UIFont(name: "AppleSDGothicNeo", size: 14)
+    }
+    
+    let emailTF = UnderLineTextField().then {
+        $0.setPlaceholder(placeholder: "이메일을 입력해주세요.")
+    }
+    
     override func addView() {
-        view.addSubview(loginTitleLabel)
+        
     }
     
     override func setLayout() {
@@ -21,14 +30,5 @@ class LoginVC: BaseVC {
             $0.centerX.equalToSuperview()
         }
     }
-    
-    override func configureVC() {
-        for fontFamily in UIFont.familyNames {
-            for fontName in UIFont.fontNames(forFamilyName: fontFamily) {
-                print(fontName)
-            }
-        }
-    }
-    
 }
 
