452db55d814b2a7a5114aeaa039b2b9c1723d598
Mindohyeon <min50875@gmail.com>
2022-07-04 08:00:47 +0900

    LaunchScreen 시간 조절

--- Source/App/AppDelegate.swift
+++ Source/App/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        sleep(3)
+        sleep(1)
         return true
     }
 
