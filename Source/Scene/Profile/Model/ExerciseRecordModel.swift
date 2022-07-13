67d610fd9b85b6203002c662a1ca7f56340c8c39
Mindohyeon <min50875@gmail.com>
2022-07-13 14:10:24 +0900

    ExerciseRecord Model 생성

---
+++ Source/Scene/Profile/Model/ExerciseRecordModel.swift
@@ -0,0 +1,13 @@
+//
+//  ExerciseRecordModel.swift
+//  HealthManager
+//
+//  Created by 민도현 on 2022/07/13.
+//  Copyright © 2022 com.tm. All rights reserved.
+//
+
+import Foundation
+
+struct ExerciseRecordModel {
+    var time: Int = 30
+}
