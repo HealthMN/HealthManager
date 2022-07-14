7a1f4d8367f9e511fa671c2c03e92247643d723e
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-07-14 15:26:22 +0900

    HMStep

---
+++ Source/App/Coordinator/Step/HMStep.swift
@@ -0,0 +1,11 @@
+enum HMStep {
+    
+    // MARK: Login
+    case loginIsRequired
+    case signUpIsRequired
+    
+    // MARK: Main
+    case mainCalendarIsRequired
+    case addAlarmIsRequired
+    case profileIsRequired
+}
