93bed0bd45da797eb77c4f4f22ff269e5d119bb7
Mindohyeon <min50875@gmail.com>
2022-07-14 15:17:51 +0900

    Graph 커스텀

--- Source/Scene/Profile/ViewControllers/ProfileGraphVC.swift
+++ Source/Scene/Profile/ViewControllers/ProfileGraphVC.swift
@@ -25,7 +25,14 @@ class ProfileGraphVC: BaseVC {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private let lineChartView = LineChartView()
+    private let lineChartView = LineChartView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 8
+        $0.xAxis.labelTextColor = .clear
+        $0.rightAxis.enabled = false
+        $0.leftAxis.labelTextColor = HealthManagerAsset.hmPrimary.color
+        $0.animate(xAxisDuration: 2)
+    }
     
     override func configureVC() {
         view.backgroundColor = .init(red: 0.97, green: 0.98, blue: 0.97, alpha: 1)
@@ -34,10 +41,14 @@ class ProfileGraphVC: BaseVC {
     
     override func viewDidLayoutSubviews() {
         viewModel.readData()
-        let set = LineChartDataSet(entries: viewModel.entries)
+        let set = LineChartDataSet(entries: viewModel.entries, label: "오늘의 운동시간")
         let data = LineChartData(dataSet: set)
-        lineChartView.data = data
         
+        set.mode = .horizontalBezier
+        set.lineWidth = 2
+        set.setColors(HealthManagerAsset.hmPrimary.color)
+        
+        lineChartView.data = data
     }
     
     override func addView() {
@@ -46,7 +57,9 @@ class ProfileGraphVC: BaseVC {
     
     override func setLayout() {
         lineChartView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.equalTo(view.safeAreaInsets.top).inset(104)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(270)
         }
     }
 }
