bee46fb19e801f6da10764f0abc5cae58ebbbe2e
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-07-20 10:59:06 +0900

    AddAlarmVM

--- Source/Scene/AddAlarm/ViewModel/AddAlarmViewModel.swift
+++ Source/Scene/AddAlarm/ViewModel/AddAlarmViewModel.swift
@@ -1,7 +1,15 @@
 import Foundation
 import RealmSwift
+import UIKit
 
 final class AddAlarmViewModel: BaseViewModel {
+    
+    private let closure: () -> Void
+    
+    init(coordinator: baseCoordinator, closure: @escaping () -> Void) {
+        self.closure = closure
+        super.init(coordinator: coordinator)
+    }
         
     func selectButtonDidTap(date: Date, title: String, icon: String, week: String, index: Int) -> Bool {
         let realm = try! Realm()
@@ -15,6 +23,13 @@ final class AddAlarmViewModel: BaseViewModel {
             realm.add(model)
         }        
         return true
+        
+    }
+    
+    func dismiss() {
+        coordinator.navigate(to: .dismiss({ [weak self] in
+            self?.closure()
+        }))
         
     }
     
