864e619b76ea14c7ff8c5f425a22e2397a8c2f1d
Mindohyeon <min50875@gmail.com>
2022-08-09 08:36:24 +0900

    코드정리

--- Source/App/Coordinator/Main/MainCoordinator.swift
+++ Source/App/Coordinator/Main/MainCoordinator.swift
@@ -3,14 +3,8 @@ import UIKit
 final class MainCoordinator: baseCoordinator {
     // MARK: - Start
     override func start() {
-//        let vm = LoginViewModel(coordinator: self)
-//        let vc = LoginVC(viewModel: vm)
-        
-        let vm = EditTimeViewModel(coordinator: self)
-        let vc = EditTimeViewController(viewModel: vm)
-        
-//        let vm = CalendarViewModel(coordinator: self)
-//        let vc = MainCalendarVC(viewModel: vm)
+        let vm = LoginViewModel(coordinator: self)
+        let vc = LoginVC(viewModel: vm)
         
         self.nav.setViewControllers([vc], animated: true)
     }
