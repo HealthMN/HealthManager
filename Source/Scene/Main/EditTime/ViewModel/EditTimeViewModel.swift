d87a20274109352e37a32dc95727b01f1d9161ef
Mindohyeon <min50875@gmail.com>
2022-08-12 23:43:47 +0900

    EditTimeRecord 함수에 변수생성

--- Source/Scene/Main/EditTime/ViewModel/EditTimeViewModel.swift
+++ Source/Scene/Main/EditTime/ViewModel/EditTimeViewModel.swift
@@ -1,3 +1,16 @@
+import UIKit
+import RealmSwift
+
 class EditTimeViewModel: BaseViewModel {
     
+    // MARK: - properties
+    let realm = try! Realm()
+    
+    func editTimeRecord(time: Int) {
+        let realm = try! Realm()
+        let editExerciseRecord = realm.objects(ProfileModel.self).toArray()
+
+        var exerciseRecord = ProfileModel(time: time)
+        let timeRecordLastIndex = editExerciseRecord.count - 1
+    }
 }
