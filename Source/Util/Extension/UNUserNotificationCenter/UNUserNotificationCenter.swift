e1ed8e204489df219fa8e5aa33006b65fa521343
Mindohyeon <min50875@naver.com>
2022-09-21 14:18:18 +0900

    Delete Print

--- Source/Util/Extension/UNUserNotificationCenter/UNUserNotificationCenter.swift
+++ Source/Util/Extension/UNUserNotificationCenter/UNUserNotificationCenter.swift
@@ -1,14 +1,5 @@
-//
-//  UNUserNotificationCenter.swift
-//  HealthManager
-//
-//  Created by 민도현 on 2022/09/13.
-//  Copyright © 2022 com.dohyeon. All rights reserved.
-//
-
 import Foundation
 import UserNotifications
-import SwiftUI
 
 extension UNUserNotificationCenter {
     func addNotificationRequest(by alert: Alarm, body: String) {
@@ -22,7 +13,6 @@ extension UNUserNotificationCenter {
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: true)
         
-
         let request = UNNotificationRequest(identifier: alert.id, content: content, trigger: trigger)
         self.add(request, withCompletionHandler: nil)
         print("add!")
