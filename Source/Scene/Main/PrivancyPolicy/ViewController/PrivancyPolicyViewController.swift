ed9039390b29635376dcf19174b2fb2f84cc775f
Mindohyeon <min50875@naver.com>
2022-09-23 10:02:27 +0900

    ScrollView -> TextView

--- Source/Scene/Main/PrivancyPolicy/ViewController/PrivancyPolicyViewController.swift
+++ Source/Scene/Main/PrivancyPolicy/ViewController/PrivancyPolicyViewController.swift
@@ -4,40 +4,21 @@ import SnapKit
 
 class PrivancyPolicyViewController: BaseVC<PrivancyPolicyViewModel> {
     
-    private let contentScrollView = UIScrollView().then {
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.showsVerticalScrollIndicator = false
-    }
-    
-    private let contentView = UIView()
-    
     private let titleLabel = UILabel().then {
         $0.text = "개인정보처리방침"
+        $0.font = .boldSystemFont(ofSize: 20)
     }
     
-    private let privancyPolicyLabel = UILabel().then {
+    private let privancyPolicyLabel = UITextView().then {
         $0.font = .systemFont(ofSize: 12)
-        $0.numberOfLines = 0
-//        $0.sizeToFit()
+        $0.isEditable = false
     }
     
     override func addView() {
-        view.addSubview(contentScrollView)
-        contentScrollView.addSubview(contentView)
-        contentView.addSubviews(titleLabel, privancyPolicyLabel)
+        view.addSubviews(titleLabel, privancyPolicyLabel)
     }
     
     override func setLayout() {
-        contentScrollView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-        
-        contentView.snp.makeConstraints {
-            $0.width.equalToSuperview()
-//            $0.centerX.top.bottom.equalToSuperview()
-            $0.edges.equalToSuperview()
-        }
-        
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.equalToSuperview().inset(30)
@@ -53,7 +34,6 @@ class PrivancyPolicyViewController: BaseVC<PrivancyPolicyViewModel> {
     override func configureVC() {
         viewModel.readTxtFile()
         privancyPolicyLabel.text = viewModel.result
-        contentScrollView.updateContentSize()
         print(viewModel.readTxtFile())
     }
 }
