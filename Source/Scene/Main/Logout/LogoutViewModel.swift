abb1a164715e309607c3e297a948b536058fa1c9
Mindohyeon <min50875@naver.com>
2022-10-18 16:59:13 +0900

    변수삭제

--- Source/Scene/Main/Logout/LogoutViewModel.swift
+++ Source/Scene/Main/Logout/LogoutViewModel.swift
@@ -4,13 +4,12 @@ import Firebase
 class LogoutViewModel: BaseViewModel {
     func presentToLogoutAlert() {
         let sheet = UIAlertController(title: "로그아웃", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
-        let firebase = Auth.auth()
         
         sheet.addAction(UIAlertAction(title: "네", style: .destructive, handler: { [weak self] value in
             if value.isEnabled {
                 print("ok")
                 do {
-                    try firebase.signOut()
+                    try Auth.auth().signOut()
                 } catch(let error as NSError) {
                     print("error = \(error)")
                 }
